a5bf1ed8c26e298c282cf751097cd3a054263c4f
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-01 15:32:07 +0100

    Update while.py

--- Iteration/while.py
+++ Iteration/while.py
@@ -1,25 +1,30 @@
-# 1. assign variable total to store sum
-# 2. assign variable count to store number of entries
-# 3. request user to enter few numbers and assign them to variable num
-# 4. initiate a while loop, check if num is not equal to -1
-# 5. update total with inputted number, num
-# 6. increment count by one
-# 7. continue asking input until user enters -1
-# 8. intiate if condition if user enters -1
-# 9. calculate average = total/count and store in variable avg
-# 10. print total and average using f string and exit while loop, using break
-
-
+# 1. Assign variable 'total' to store the sum of the numbers
 total = 0
+
+# 2. Assign variable 'count' to store the number of entries
 count = 0
-num = int(input('Enter a few integers and end by typing -1 :'))
 
+# 3. Request user to enter a few numbers and assign the first input to variable 'num'
+num = int(input('Enter a few integers and end by typing -1: '))
+
+# 4. Initiate a while loop that continues as long as 'num' is not equal to -1
 while num != -1:
-    total += num
-    count += 1
-    num = int(input('Enter a few integers and end by typing -1 :'))
 
-    if num == -1:
-        avg = total/count
-        print(f' Total of numbers (excluding -1) entered is {total} and the average is {avg}')
-        break
+# 5. Update 'total' by adding the inputted number 'num'
+total += num
+    
+# 6. Increment 'count' by one to keep track of the number of valid entries
+count += 1
+    
+# 7. Continue asking for input until the user enters -1
+num = int(input('Enter a few integers and end by typing -1: '))
+
+# 8. Initiate if condition to check if the user enters -1
+if num == -1:
+
+# 9. Calculate the average by dividing total by count and store it in variable 'avg'
+avg = total / count
+        
+# 10. Print the total and average using an f-string and exit the while loop using break
+print(f'Total of numbers (excluding -1) entered is {total} and the average is {avg}')
+break
