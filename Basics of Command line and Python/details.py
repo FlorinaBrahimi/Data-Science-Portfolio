0431a75cb43879f22e50b3570648f0085c0f3350
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-01 15:20:11 +0100

    Update details.py

--- Basics of Command line and Python/details.py
+++ Basics of Command line and Python/details.py
@@ -1,20 +1,15 @@
-# kept all code clean as possible using PEP 8 style guide 😊
-# request user to input name
-# request user to input age
-# request user to input house number
-# request user to input street name
-# print welcome statement with name, age, house number and street name
-# using str.format() method to add variables all in a single sentence
-
-
+# Prompt the user to input their name and store it in the variable 'name'
 name = input("What is your name : ")
 
+# Prompt the user to input their age, convert it to an integer, and store it in the variable 'age'
 age = int(input("What is your age : "))
 
+# Prompt the user to input their house number, convert it to an integer, and store it in the variable 'house_number'
 house_number = int(input("What is your house number : "))
 
+# Prompt the user to input their street name and store it in the variable 'street_name'
 street_name = input("What is your street name : ")
 
-
+# Print a greeting message that includes the user's name, age, house number, and street name
 print("Hello {0}! Welcome! You are {1} years old and live at {2} {3}.".format(
     name, age, house_number, street_name))
