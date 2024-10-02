e55c0eaffafa8653bc0f15b9c68cded118462b2b
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 21:53:25 +0100

    Update Difference.py

--- Condition/Difference.py
+++ Condition/Difference.py
@@ -1,7 +1,13 @@
+# Prompt the user to enter the first number and convert the input to an integer
 no1 = int(input('Enter first number'))
+
+# Prompt the user to enter the second number and convert the input to an integer
 no2 = int(input('Enter second number'))
 
-if no1 - no2 >=0:
-    print(no1-no2)
+# Check if the difference between no1 and no2 is greater than or equal to 0
+if no1 - no2 >= 0:
+    # If true, print the result of no1 minus no2
+    print(no1 - no2)
 else:
-    print(no2-no1)
+    # If false (i.e., no2 is greater), print the result of no2 minus no1
+    print(no2 - no1)
