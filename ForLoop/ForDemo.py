647550d642f663cbafd2b9e02c2ca3e0b03f8570
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 22:18:21 +0100

    Update ForDemo.py

--- ForLoop/ForDemo.py
+++ ForLoop/ForDemo.py
@@ -1,3 +1,5 @@
-
-for i in range(10,0,-1):
+# Loop through numbers starting from 10 down to 1 (inclusive)
+# The range(10, 0, -1) generates numbers starting at 10 and decreases by 1 with each iteration until it reaches 1
+for i in range(10, 0, -1):
+    # Print the current value of 'i'
     print(i)
