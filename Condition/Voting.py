4e4c8757c4111dfc49b14a58c5e9c6a29c9c5795
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 21:58:19 +0100

    Update Voting.py

--- Condition/Voting.py
+++ Condition/Voting.py
@@ -1,8 +1,11 @@
+# Prompt the user to enter their age and convert the input to an integer
 age = int(input('Enter your age'))
 
+# Check if the age is 18 or above
 if age >= 18:
+    # If the age is 18 or more, print 'Eligible'
     print('Eligible')
+# If the age is less than 18, print 'Not Eligible'
 else:
+    # Print 'Not Eligible' for those under 18
     print('Not Eligible')
-
-    
