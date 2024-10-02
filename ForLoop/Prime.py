16e6d2fec772a29da26c7b439c7f305ce7070a6b
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 22:20:39 +0100

    Update Prime.py

--- ForLoop/Prime.py
+++ ForLoop/Prime.py
@@ -1,13 +1,18 @@
+# Prompt the user to enter a number and convert it to an integer
+n = int(input('Enter a Number: '))
 
-n = int(input('Enter a Number'))
-
+# Initialize a variable 'count' to keep track of the number of divisors of n
 count = 0
 
+# Loop through numbers from 1 to n (inclusive) to check for divisors
 for i in range(1, n+1):
+    # If n is divisible by i (remainder is 0), increase the divisor count
     if n % i == 0:
         count += 1
 
+# If the number has exactly 2 divisors (1 and itself), it is a prime number
 if count == 2:
-    print('Its a Prime')
+    print('It\'s a Prime')
 else:
-    print('Its Not a Prime')
+    # If the number has more than 2 divisors, it is not a prime number
+    print('It\'s Not a Prime')
