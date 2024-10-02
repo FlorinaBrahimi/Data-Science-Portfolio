b517bc35c7d045b63a663faed67d211f2d61e5b6
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 22:36:24 +0100

    Update FunctionSC5.py

--- FunctionChallenge/FunctionSC5.py
+++ FunctionChallenge/FunctionSC5.py
@@ -1,11 +1,14 @@
-
-
+# Define a function 'max3' that returns the maximum of three numbers
 def max3(a, b, c):
+    # If 'a' is greater than both 'b' and 'c', return 'a' as the maximum
     if a > b and a > c:
         return a
+    # If 'b' is greater than 'c', return 'b' as the maximum
     elif b > c:
         return b
+    # If neither of the above conditions are met, return 'c' as the maximum
     else:
         return c
 
-print(max3(12,12,12))
+# Test the function with the input (12, 12, 12)
+print(max3(12, 12, 12))
