358122d26cfbeeecc95d396c776011d2464b5124
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 22:13:49 +0100

    Update Rectangle.py

--- Expressions/Rectangle.py
+++ Expressions/Rectangle.py
@@ -1,4 +1,12 @@
-length = int(input('Enter length of  Rectangle'))
-breadth = int(input('Enter breadth of a rectangle'))
+# Prompt the user to enter the length of the rectangle and convert the input to an integer
+length = int(input('Enter length of Rectangle: '))
+
+# Prompt the user to enter the breadth of the rectangle and convert the input to an integer
+breadth = int(input('Enter breadth of Rectangle: '))
+
+# Calculate the area of the rectangle using the formula: area = length * breadth
 area = length * breadth
-print('Area is ', area)
+
+# Print the calculated area of the rectangle
+# The output will display the area with the message 'Area is'
+print('Area is', area)
