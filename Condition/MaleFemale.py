72aa7b8a5211d357339923f15d03c52c523b9594
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 21:55:45 +0100

    Update MaleFemale.py

--- Condition/MaleFemale.py
+++ Condition/MaleFemale.py
@@ -1,6 +1,10 @@
+# Prompt the user to enter gender ('m' or 'M' for male, anything else is considered female)
 gender = input('Enter Gender')
 
-if gender=='m' or gender=='M':
+# Check if the entered gender is 'm' or 'M' (case insensitive for male)
+if gender == 'm' or gender == 'M':
+    # If the input is 'm' or 'M', print 'Male'
     print('Male')
+# If the input is not 'm' or 'M', it is considered 'Female'
 else:
     print('Female')
