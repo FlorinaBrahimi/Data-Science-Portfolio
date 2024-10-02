2f1ed77aa682abbc3bafd8b9eaa0b27c7fb35587
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 22:37:14 +0100

    Update FunctionSC6.py

--- FunctionChallenge/FunctionSC6.py
+++ FunctionChallenge/FunctionSC6.py
@@ -1,4 +1,5 @@
 # Define a function 'message' that takes two positional-only arguments: 'name' and 'prof'
+
 def message(name, prof, /):
     # Print a formatted message with the provided name and profession
     print('My name is ' + name + ' and I am a/an ' + prof)
