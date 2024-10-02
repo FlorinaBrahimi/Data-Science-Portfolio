8d08daf0003ff3fc64383b6912377033947b4696
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 21:57:15 +0100

    Update OddEven.py

--- Condition/OddEven.py
+++ Condition/OddEven.py
@@ -1,8 +1,11 @@
+# Prompt the user to enter a number and convert the input to an integer
 n = int(input('Enter a Number'))
 
+# Check if the number is divisible by 2 (i.e., it's even)
 if n % 2 == 0:
+    # If divisible by 2, print 'Even'
     print('Even')
+# If the number is not divisible by 2, it is odd
 else:
+    # Print 'Odd'
     print('Odd')
-
-    
