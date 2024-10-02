595cf106dcd15c65d100b436c1d6a8af77207acf
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 22:19:41 +0100

    Update NestedLoop.py

--- ForLoop/NestedLoop.py
+++ ForLoop/NestedLoop.py
@@ -1,9 +1,13 @@
+# Define two strings, s1 and s2
+s1 = 'abc'
+s2 = 'xyz'
 
-s1='abc'
-s2='xyz'
-
+# Outer loop: Iterate through each character in s1
 for i in s1:
+    # Inner loop: For each character in s1, iterate through each character in s2
     for j in s2:
+        # Print the current characters from s1 and s2 on the same line with a space
         print(i, j, end=' ')
+    
+    # After printing all combinations of i with characters from s2, move to a new line
     print('')
-
