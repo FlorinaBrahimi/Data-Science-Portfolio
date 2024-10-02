db4cddaa1a8720b476ed41c3f4bae5416ec5b257
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 22:13:21 +0100

    Update Quadratic.py

--- Expressions/Quadratic.py
+++ Expressions/Quadratic.py
@@ -1,8 +1,21 @@
+# Import the 'math' module to access mathematical functions like square root
 import math
 
-a = int(input('Enter a value'))
-b = int(input('Enter b value'))
-c = int(input('Enter c value'))
-root1 = (-b + math.sqrt(b**2 - 4 * a * c))/(2 * a)
-root2 = (-b - math.sqrt(b**2 - 4 * a * c))/(2 * a)
-print('Roots are ', root1, root2)
+# Prompt the user to enter the value of 'a' and convert the input to an integer
+a = int(input('Enter a value: '))
+
+# Prompt the user to enter the value of 'b' and convert the input to an integer
+b = int(input('Enter b value: '))
+
+# Prompt the user to enter the value of 'c' and convert the input to an integer
+c = int(input('Enter c value: '))
+
+# Calculate the first root using the quadratic formula: (-b + sqrt(b^2 - 4ac)) / 2a
+root1 = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+
+# Calculate the second root using the quadratic formula: (-b - sqrt(b^2 - 4ac)) / 2a
+root2 = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+
+# Print the two roots of the quadratic equation
+# The output will display both roots with the message 'Roots are'
+print('Roots are', root1, root2)
