45f72b06d34c660d9726bb4b9fac4b8e966c3ed7
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 22:22:55 +0100

    Update FindString.py

--- ListChallenges/FindString.py
+++ ListChallenges/FindString.py
@@ -1,10 +1,12 @@
-
-
+# Define a list of food items
 food = ['pizza', 'nuggets', 'hotdog', 'noodles', 'pasta', 'burger']
 
-letter = input('Enter a Letter')
+# Prompt the user to enter a letter
+letter = input('Enter a Letter: ')
 
+# Loop through each item in the food list
 for x in food:
+    # Check if the current food item starts with the letter entered by the user
     if x.startswith(letter):
+        # If it does, print the food item
         print(x)
-
