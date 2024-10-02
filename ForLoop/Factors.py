3affadc9b6262f0b2a748fc69fdc779de0b3011c
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 22:16:43 +0100

    Update Factors.py

--- ForLoop/Factors.py
+++ ForLoop/Factors.py
@@ -1,5 +1,9 @@
+# Prompt the user to enter a number and convert the input to an integer
+n = int(input('Enter a Number: '))
 
-n = int(input('Enter a Number'))
+# Loop through the numbers from 1 to n (inclusive) to find the divisors
 for i in range(1, n+1):
+    # Check if the current number 'i' is a divisor of 'n' by checking if the remainder is 0
     if n % i == 0:
+        # If 'i' is a divisor, print it
         print(i)
