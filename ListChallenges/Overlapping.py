697c82d8ab8e20ff05985e99797ffe266b3ef932
Florina.Brahimi <160019246+FlorinaBrahimi@users.noreply.github.com>
2024-10-02 22:27:06 +0100

    Update Overlapping.py

--- ListChallenges/Overlapping.py
+++ ListChallenges/Overlapping.py
@@ -1,12 +1,16 @@
-
+# Define two lists, L1 and L2
 L1 = [10, 15, 6, 9, 12, 8, 4]
-
 L2 = [14, 6, 19 , 4, 7, 10, 11]
 
+# Initialize an empty list to store the common elements
 L3 = []
 
+# Loop through each element in L1
 for x in L1:
+    # Check if the current element 'x' is also in L2
     if x in L2:
+        # If it's in both L1 and L2, append it to L3
         L3.append(x)
 
+# Print the list of common elements
 print(L3)
